d90462501ca31e83b6994cc85e9e85d571988267
Narayan Poudel <poudelnarayan434@gmail.com>
2025-07-23 03:22:14 +0545

    handling 'not  found' page

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,8 +1,13 @@
 import { NEWS } from "@/public/DUMMY/news-data";
+import { notFound } from "next/navigation";
 
 export default function NewsDetailsPage({ params }) {
   const { slug } = params;
   const news = NEWS.find((newsItm) => newsItm.slug === slug);
+
+  if (!news) {
+    notFound();
+  }
   return (
     <article className="news-article">
       <header>
